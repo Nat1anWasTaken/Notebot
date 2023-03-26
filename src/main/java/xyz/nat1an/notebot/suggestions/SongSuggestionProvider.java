2b2a3abe5faf705f213c1451572e86520c4d734e
Nat1an <nathan@nat1an.xyz>
2023-03-26 13:30:02 +0800

    Dealing with licenses before open source

--- src/main/java/xyz/nat1an/notebot/suggestions/SongSuggestionProvider.java
+++ src/main/java/xyz/nat1an/notebot/suggestions/SongSuggestionProvider.java
@@ -1,3 +1,10 @@
+/*
+This file is part of Notebot.
+Notebot is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Notebot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Notebot. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package xyz.nat1an.notebot.suggestions;
 
 import com.mojang.brigadier.context.CommandContext;
